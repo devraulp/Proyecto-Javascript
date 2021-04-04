6f4efcadadcb0ee398d0c2e1107053dd9c50aa44
Raul Paredes <raul_paredes_s@hotmail.com>
2021-04-04 20:43:19 -0300

    jquery

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// --------------------- CLASS DONDE INGRESAN LOS DATOS ----------------------------
+
+class dato {
+    constructor (/* mes ,*/tipo,/* cuotas ,*/nombre,monto,moneda) {
+        // this.mes = mes;
+        this.tipo = tipo;
+        // this.cuotas = cuotas;
+        this.nombre = nombre;
+        this.monto = monto;
+        this.moneda = moneda;
+    }
+}
+
+// --------------------- ARRAY DE LA CLASS dato ------------------------------------
+
+let datos = [];
+let obtenerDato;
+
+// --------------------- VARIABLES HTML --------------------------------------------
+
+let datoTipo = $("#tipo").on("change", (e) =>{datoTipo = e.target.value});
+let datoNombre = $("#nombre").on("change", (e) =>{datoNombre = e.target.value});
+let datoMonto = $("#monto").on("change", (e) =>{datoMonto = e.target.value});
+let datoMoneda = $("#moneda").on("change", (e) =>{datoMoneda = e.target.value});
+// let datoCuotas = document.getElementById("numCuotas");
+// let datoMes = document.getElementById("mes");
+
 //-------------------- CONDICIONAL PARA MOSTRAR LOS DATOS DEL STORAGE  ------------
 
 if (localStorage.getItem("memoria") === null){
@@ -9,31 +36,31 @@ if (localStorage.getItem("memoria") === null){
 
 // --------------------- EVENTO PARA CREAR OBJETO Y MOSTRARLO ---------------------
 
-btnIngresar.addEventListener("click", () => { 
+$("#btnIngresar").on("click", () =>{
     crearDato()
     imprimirDato()
-});
+})
 
 // --------------------- EVENTO PARA GUARDAR DATOS  -------------------------------
 
-btnGuardar.addEventListener("click", () => {     
-    localStorage.setItem("memoria", JSON.stringify(datos)); 
-});
+$("#btnGuardar").on("click", () => {
+    localStorage.setItem("memoria", JSON.stringify(datos));
+})
 
 // --------------------- EVENTO PARA ELIMINAR DATOS  -----------------------------
 
-btnEliminar.addEventListener("click", () => {     
+$("#btnEliminar").on("click", () => {
     localStorage.clear(); 
     location.reload();
-});
+})
 
 //-------------------- FUNCION PARA OBTENER LOS DATOS DE LOS INPUT----------------------
 
 function crearDato () {   
-    obtenerDato = new dato(/* datoMes.value ,*/ datoTipo.value, 
-                                /* datoCuotas.value, */ datoNombre.value, 
-                                datoMonto.value, datoMoneda.value);
-    datos.push(obtenerDato);   
+    obtenerDato = new dato(/* datoMes.value ,*/ datoTipo, 
+                                /* datoCuotas.value, */ datoNombre, 
+                                datoMonto, datoMoneda);
+    datos.push(obtenerDato);
 }  
 
 //-------------------- FUNCION PARA MOSTRAR LOS DATOS  --------------------------------
@@ -82,79 +109,83 @@ function imprimirDato() {
             if (elem.moneda == "ARS" && elem.monto > 0) {
                 ingresarIngreso(4,5);
                 sumaIngresosARS = parseInt(elem.monto) + sumaIngresosARS;
-                totalIngresoARS.innerHTML = sumaIngresosARS + " " +  "ARS";
+                $("#totalIngresoARS").html(sumaIngresosARS + " " +  "ARS");
             } else if (elem.moneda == "BRL" && elem.monto > 0){
                 ingresarIngreso(5,4);
                 sumaIngresosBRL = parseInt(elem.monto) + sumaIngresosBRL; 
-                totalIngresoBRL.innerHTML = sumaIngresosBRL + " " + "BRL";
+                $("#totalIngresoBRL").html(sumaIngresosBRL + " " + "BRL");
             }else if (elem.moneda == "UYU" && elem.monto > 0){
                 ingresarIngreso(6,3)
                 sumaIngresosUYU = parseInt(elem.monto) + sumaIngresosUYU; 
-                totalIngresoUYU.innerHTML = sumaIngresosUYU + " " + "UYU";
+                $("#totalIngresoUYU").html(sumaIngresosUYU + " " + "UYU");
             }else if (elem.moneda == "USD" && elem.monto > 0){
                 ingresarIngreso(7,2);
                 sumaIngresosUSD = parseInt(elem.monto) + sumaIngresosUSD;
-                totalIngresoUSD.innerHTML = sumaIngresosUSD + " " + "USD";
+                $("#totalIngresoUSD").html(sumaIngresosUSD + " " + "USD");
             }else if (elem.moneda == "EUR" && elem.monto > 0){
                 ingresarIngreso(8,1);
                 sumaIngresosEUR = parseInt(elem.monto) + sumaIngresosEUR; 
-                totalIngresoEUR.innerHTML = sumaIngresosEUR + " " + "EUR"; 
+                $("#totalIngresoEUR").html(sumaIngresosEUR + " " + "EUR"); 
             }
         }else if(elem.tipo == "Gasto") {
             
             if (elem.moneda == "ARS" && elem.monto > 0) {
                 ingresarGasto(4,5);
                 sumaGastosARS = parseInt(elem.monto) + sumaGastosARS;
-                totalGastoARS.innerHTML = sumaGastosARS + " " + "ARS";                           
+                $("#totalGastoARS").html(sumaGastosARS + " " + "ARS");                           
             } else if (elem.moneda == "BRL" && elem.monto > 0){
                 ingresarGasto(5,4);
                 sumaGastosBRL = parseInt(elem.monto) + sumaGastosBRL;
-                totalGastoBRL.innerHTML = sumaGastosBRL + " " + " BRL"; 
+                $("#totalGastoBRL").html(sumaGastosBRL + " " + " BRL"); 
             }else if (elem.moneda == "UYU" && elem.monto > 0){
                 ingresarGasto(6,3);
                 sumaGastosUYU = parseInt(elem.monto) + sumaGastosUYU;
-                totalGastoUYU.innerHTML = sumaGastosUYU + " " + " UYU"; 
+                $("#totalGastoUYU").html(sumaGastosUYU + " " + " UYU"); 
             }else if (elem.moneda == "USD" && elem.monto > 0){
                 ingresarGasto(7,2);
                 sumaGastosUSD = parseInt(elem.monto) + sumaGastosUSD;
-                totalGastoUSD.innerHTML = sumaGastosUSD + " " + " USD";  
+                $("#totalGastoUSD").html(sumaGastosUSD + " " + " USD");  
             }else if (elem.moneda == "EUR" && elem.monto > 0){
                 ingresarGasto(8,1);
                 sumaGastosEUR = parseInt(elem.monto) + sumaGastosEUR;
-                totalGastoEUR.innerHTML = sumaGastosEUR + " " + " EUR"; 
+                $("#totalGastoEUR").html(sumaGastosEUR + " " + " EUR"); 
             }
         };
         
         if (sumaGastosARS > 0 && sumaIngresosARS > 0){
-        balanceGeneralARS = parseInt(sumaIngresosARS) - parseInt(sumaGastosARS);
-        totalBalanceARS.innerHTML = balanceGeneralARS + " " + "ARS"; 
+            balanceGeneralARS = parseInt(sumaIngresosARS) - parseInt(sumaGastosARS);
+            $("#totalBalanceARS").html(balanceGeneralARS + " " + "ARS"); 
         }
 
         if (sumaIngresosBRL > 0 && sumaGastosBRL > 0) {
             balanceGeneralBRL = parseInt(sumaIngresosBRL) - parseInt(sumaGastosBRL);
-            totalBalanceBRL.innerHTML = balanceGeneralBRL + " " + " BRL";
+            $("#totalBalanceBRL").html(balanceGeneralBRL + " " + " BRL");
         }
         
         if (sumaIngresosUYU > 0 && sumaGastosUYU > 0) {
             balanceGeneralUYU = parseInt(sumaIngresosUYU) - parseInt(sumaGastosUYU);
-        totalBalanceUYU.innerHTML = balanceGeneralUYU + " " + " UYU";
+            $("#totalBalanceUYU").html(balanceGeneralUYU + " " + " UYU");
         }
         
         if (sumaIngresosUSD > 0 && sumaGastosUSD > 0) {
             balanceGeneralUSD = parseInt(sumaIngresosUSD) - parseInt(sumaGastosUSD);
-            totalBalanceUSD.innerHTML = balanceGeneralUSD + " " + " USD";
+            $("#totalBalanceUSD").html(balanceGeneralUSD + " " + " USD");
         }
         
         if (sumaIngresosEUR > 0 && sumaGastosEUR > 0) {
             balanceGeneralEUR = parseInt(sumaIngresosEUR) - parseInt(sumaGastosEUR);
-            totalBalanceEUR.innerHTML = balanceGeneralEUR + " " + " EUR";
+            $("#totalBalanceEUR").html(balanceGeneralEUR + " " + " EUR");
         }
     }
+
+    
+
     ingreso = ingreso + `</table>`;
     gasto = gasto + `</table>`;
-    imprimirIngreso.innerHTML = ingreso;
-    imprimirGasto.innerHTML = gasto; 
+    $("#ingreso").html(ingreso);
+    $("#gasto").html(gasto);
 }
+
 
 // //-------------------- FUNCION PARA MOSTRAR EL INPUT CUOTAS----------------------
 
